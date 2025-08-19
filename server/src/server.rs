b30eca81c412d3bd1669c770d4fa71a6f6a73e58
Nico812 <118693240+Nico812@users.noreply.github.com>
2025-08-19 14:09:59 +0200

    f

--- server/src/server.rs
+++ server/src/server.rs
@@ -7,14 +7,13 @@
 use std::sync::{Arc, Mutex};
 use std::thread;
 use tokio::io::BufReader;
-use tokio::net::{TcpListener, TcpStream, tcp::{OwnedReadHalf, OwnedWriteHalf}};
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf, TcpListener};
 use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
 
 use crate::lobby;
 use common::r#const::{IP_LOCAL, MAX_LOBBIES, ONLINE};
 use common::{self, stream};
 
-/// Messages sent from the main `Server` to a `Lobby` thread.
 pub enum S2L {
     IsFull(mpsc::Sender<bool>),
     NewClient(
@@ -24,7 +23,6 @@ pub enum S2L {
     ),
     Shutdown,
 }
-
 #[derive(Debug)]
 pub enum ServerErr {
     PoisonedMutex,
@@ -32,246 +30,212 @@ pub enum ServerErr {
     ServerFull,
     AuthFailed,
 }
-
 pub type ClientID = usize;
 
-pub struct Server {
+//==============================================================================================
+//  Lobby Manager
+//==============================================================================================
+
+/// Manages the entire lifecycle of game lobbies.
+struct LobbyManager {
     threads: Arc<Mutex<[Option<thread::JoinHandle<()>>; MAX_LOBBIES]>>,
     lobby_txs: Arc<Mutex<[Option<mpsc::UnboundedSender<S2L>>; MAX_LOBBIES]>>,
 }
 
-impl Server {
-    pub fn new() -> Self {
-        let threads = Arc::new(Mutex::new([const { None }; MAX_LOBBIES]));
-        let lobby_txs = Arc::new(Mutex::new([const { None }; MAX_LOBBIES]));
-
-        Self { threads, lobby_txs }
+impl LobbyManager {
+    fn new() -> Self {
+        Self {
+            threads: Arc::new(Mutex::new([const { None }; MAX_LOBBIES])),
+            lobby_txs: Arc::new(Mutex::new([const { None }; MAX_LOBBIES])),
+        }
     }
 
-    /// Runs the main server loop.
-    ///
-    /// Listens for incoming TCP connections and spawns a new task to handle each client.
+    /// Finds an available lobby or creates a new one for a client.
+    async fn assign_client_to_lobby(
+        &self,
+        client_id: ClientID,
+    ) -> Result<(UnboundedSender<common::C2S4L>, UnboundedReceiver<common::L2S4C>), ServerErr> {
+        // First, check for an existing lobby with space
+        for i in 0..MAX_LOBBIES {
+            let lobby_tx = self.lobby_txs.lock().unwrap()[i].clone();
+            if let Some(tx) = lobby_tx {
+                let (resp_tx, mut resp_rx) = mpsc::channel(1);
+                let _ = tx.send(S2L::IsFull(resp_tx));
+                if let Some(is_full) = resp_rx.recv().await {
+                    if !is_full {
+                        let (c2s_tx, c2s_rx) = mpsc::unbounded_channel();
+                        let (s2c_tx, s2c_rx) = mpsc::unbounded_channel();
+                        let _ = tx.send(S2L::NewClient(client_id, s2c_tx, c2s_rx));
+                        return Ok((c2s_tx, s2c_rx));
+                    }
+                }
+            }
+        }
+
+        // If no lobby has space, try to create a new one
+        let mut threads_guard = self.threads.lock().unwrap();
+        let mut lobby_txs_guard = self.lobby_txs.lock().unwrap();
+        for i in 0..MAX_LOBBIES {
+            if threads_guard[i].is_none() {
+                let (lobby_tx, lobby_rx) = mpsc::unbounded_channel();
+                let mut lobby = lobby::Lobby::new();
+                
+                threads_guard[i] = Some(thread::spawn(move || {
+                    let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
+                    rt.block_on(async move { lobby.run(lobby_rx).await });
+                }));
+                
+                lobby_txs_guard[i] = Some(lobby_tx.clone());
+
+                let (c2s_tx, c2s_rx) = mpsc::unbounded_channel();
+                let (s2c_tx, s2c_rx) = mpsc::unbounded_channel();
+                let _ = lobby_tx.send(S2L::NewClient(client_id, s2c_tx, c2s_rx));
+                return Ok((c2s_tx, s2c_rx));
+            }
+        }
+
+        Err(ServerErr::ServerFull)
+    }
+}
+
+
+//==============================================================================================
+//  Client Handler
+//==============================================================================================
+
+/// Manages the connection and communication for a single authenticated client.
+struct ClientHandler {
+    client_id: ClientID,
+    reader: BufReader<OwnedReadHalf>,
+    writer: OwnedWriteHalf,
+    lobby_tx: UnboundedSender<common::C2S4L>,
+    lobby_rx: UnboundedReceiver<common::L2S4C>,
+}
+
+impl ClientHandler {
+    /// Runs the main communication loop for the client.
+    async fn run(mut self) {
+        loop {
+            tokio::select! {
+                result = stream::get_msg_from_client(&mut self.reader) => {
+                    match result {
+                        Ok(common::C2S::C2S4L(msg)) => {
+                            if self.lobby_tx.send(msg).is_err() {
+                                break;
+                            }
+                        },
+                        Ok(_) => {},
+                        Err(_) => {
+                            eprintln!("CLIENT (ID: {}) DISCONNECTED.", self.client_id);
+                            break;
+                        }
+                    }
+                },
+                Some(msg) = self.lobby_rx.recv() => {
+                    let s2c_msg = common::S2C::L2S4C(msg);
+                    if stream::send_msg_to_client(&mut self.writer, &s2c_msg).await.is_err() {
+                        break; // Failed to send to client
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+//==============================================================================================
+//  Server
+//==============================================================================================
+
+pub struct Server {
+    lobby_manager: Arc<LobbyManager>,
+    next_client_id: Arc<Mutex<ClientID>>,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        Self {
+            lobby_manager: Arc::new(LobbyManager::new()),
+            next_client_id: Arc::new(Mutex::new(0)),
+        }
+    }
+
     pub async fn run(&mut self) {
-        let listener;
-        if ONLINE {
-            //listener = TcpListener::bind(IP_4_SERVER).await.unwrap();
-            listener = TcpListener::bind(IP_LOCAL).await.unwrap();
-        } else {
-            listener = TcpListener::bind(IP_LOCAL).await.unwrap();
-        };
+        let listener = TcpListener::bind(IP_LOCAL).await.unwrap();
+        println!("Server started and listening on {}", IP_LOCAL);
 
-        let mut client_id_cnt = 0;
+        while let Ok((stream, socket_addr)) = listener.accept().await {
+            println!("Connection established from: {}", socket_addr);
 
-        while let Ok((mut stream, socket_addr)) = listener.accept().await {
-            println!("Connection established, socket address:, {}", socket_addr);
-
-            let threads_copy = self.threads.clone();
-            let lobby_txs_copy = self.lobby_txs.clone();
+            let lobby_manager_clone = Arc::clone(&self.lobby_manager);
+            let next_client_id_clone = Arc::clone(&self.next_client_id);
 
             tokio::spawn(async move {
                 let (reader, mut writer) = stream.into_split();
                 let mut buf_reader = BufReader::new(reader);
-                // Autentication
-                let authentication = match Self::wait_authentication(&mut buf_reader).await {
+
+                // 1. Authenticate
+                let auth_result = Self::wait_authentication(&mut buf_reader).await;
+                let user_name = match auth_result {
                     Ok(name) => {
                         println!("[{}] Player '{}' authenticated.", socket_addr, name);
                         name
                     }
                     Err(_) => {
-                        eprintln!(
-                            "[{}] Authentication failed. Closing connection.",
-                            socket_addr
-                        );
-                        let _ =
-                            stream::send_msg_to_client(&mut writer, &common::S2C::ConnectionFailed)
-                                .await;
+                        eprintln!("[{}] Authentication failed.", socket_addr);
+                        let _ = stream::send_msg_to_client(&mut writer, &common::S2C::ConnectionFailed).await;
                         return;
                     }
                 };
-                let client_id = 1;
-                client_id_cnt += client_id_cnt;
-                // Send the player to the lobby
-                match Self::handle_client(threads_copy, lobby_txs_copy, &authentication, client_id)
-                    .await
-                {
-                    Ok((client_id, client_tx, mut client_rx)) => loop {
-                        println!("cazzo sta succedendo");
-                        match stream::get_msg_from_client(&mut buf_reader).await {
-                            Ok(msg) => match msg {
-                                common::C2S::C2S4L(msg4l) => {
-                                    println!("got a msg");
-                                    let _ = client_tx.send(msg4l);
-                                }
-                                _ => {
-                                    println!("ricevuto messaggio inaspettato");
-                                }
-                            },
-                            Err(_) => {
-                                eprintln!("CLIENT (ID: {}) DISCONNECTED.", client_id);
-                                break;
-                            }
-                        }
 
-                        if let Some(msg) = client_rx.recv().await {
-                            match msg {
-                                common::L2S4C::GameObjs(objs) => {
-                                    let _ = stream::send_msg_to_client(
-                                        &mut writer,
-                                        &common::S2C::L2S4C(common::L2S4C::GameObjs(objs)),
-                                    )
-                                    .await;
-                                }
-                                common::L2S4C::Map(map) => {
-                                    println!("Sending map");
-                                    let _ = stream::send_msg_to_client(
-                                        &mut writer,
-                                        &common::S2C::L2S4C(common::L2S4C::Map(map)),
-                                    )
-                                    .await;
-                                }
-                                common::L2S4C::PlayerData(player_data) => {
-                                    let _ = stream::send_msg_to_client(
-                                        &mut writer,
-                                        &common::S2C::L2S4C(common::L2S4C::PlayerData(player_data)),
-                                    )
-                                    .await;
-                                }
-                            };
-                        }
-                    },
-
-                    Err(err) => {
-                        match err {
-                            ServerErr::MissingLobbyTx | ServerErr::PoisonedMutex => {
-                                let _ = stream::send_msg_to_client(
-                                    &mut writer,
-                                    &common::S2C::ConnectionFailed,
-                                )
-                                .await;
-                            }
-
-                            ServerErr::ServerFull => {
-                                let _ = stream::send_msg_to_client(
-                                    &mut writer,
-                                    &common::S2C::ServerFull,
-                                )
-                                .await;
-                            }
-                            _ => {}
-                        }
-                        eprintln!("\x1b[35mSERVER ERROR: {:?}\x1b[0m", err);
-                    }
+                // 2. Assign to Lobby
+                let client_id = {
+                    let mut id_guard = next_client_id_clone.lock().unwrap();
+                    let id = *id_guard;
+                    *id_guard += 1;
+                    id
                 };
+
+                let lobby_channels = lobby_manager_clone.assign_client_to_lobby(client_id).await;
+                match lobby_channels {
+                    Ok((lobby_tx, lobby_rx)) => {
+                        // 3. Hand off to Client Handler
+                        let handler = ClientHandler {
+                            client_id,
+                            reader: buf_reader,
+                            writer,
+                            lobby_tx,
+                            lobby_rx,
+                        };
+                        handler.run().await;
+                    }
+                    Err(err) => {
+                        let msg = if let ServerErr::ServerFull = err {
+                            common::S2C::ServerFull
+                        } else {
+                            common::S2C::ConnectionFailed
+                        };
+                        let _ = stream::send_msg_to_client(&mut writer, &msg).await;
+                        eprintln!("[{}] Failed to assign to lobby: {:?}", socket_addr, err);
+                    }
+                }
             });
         }
     }
-
-    /// Handles a single client connection.
-    ///
-    /// This function finds a suitable lobby for the client, either an existing one
-    /// with space or a new one if possible. It establishes communication channels
-    /// between the client and the lobby.
-    async fn handle_client(
-        threads: Arc<Mutex<[Option<thread::JoinHandle<()>>; MAX_LOBBIES]>>,
-        lobby_txs: Arc<Mutex<[Option<mpsc::UnboundedSender<S2L>>; MAX_LOBBIES]>>,
-        autentication: &String,
-        client_id: ClientID,
-    ) -> Result<
-        (
-            ClientID,
-            UnboundedSender<common::C2S4L>,
-            UnboundedReceiver<common::L2S4C>,
-        ),
-        ServerErr,
-    > {
-        let client_tx;
-        let client_rx;
-
-        // Checking if there's a lobby with space for the new player
-        for iter in 0..MAX_LOBBIES {
-            if threads.lock().unwrap()[iter].is_some() {
-                let (temp_tx, mut temp_rx) = mpsc::channel(100);
-
-                if let Ok(mut lobby_txs) = lobby_txs.lock() {
-                    let lobby_tx = match lobby_txs[iter].as_mut() {
-                        None => return Err(ServerErr::MissingLobbyTx),
-                        Some(lobby_tx) => lobby_tx,
-                    };
-
-                    let _ = lobby_tx.send(S2L::IsFull(temp_tx));
-                }
-
-                if temp_rx
-                    .recv()
-                    .await
-                    .is_some_and(|response| response == false)
-                {
-                    if let Ok(mut lobby_txs) = lobby_txs.lock() {
-                        let lobby_tx = match lobby_txs[iter].as_mut() {
-                            None => return Err(ServerErr::MissingLobbyTx),
-                            Some(lobby_tx) => lobby_tx,
-                        };
-
-                        let (tx1, rx1) = mpsc::unbounded_channel();
-                        let (tx2, rx2) = mpsc::unbounded_channel();
-                        client_rx = rx1;
-                        client_tx = tx2;
-
-                        let _ = lobby_tx.send(S2L::NewClient(client_id, tx1, rx2));
-                        println!("Found an existing lobby with space for the client");
-                        return Ok((client_id, client_tx, client_rx));
-                    }
-                }
-            }
-        }
-        // Checking if there's a lobby uninitialized
-        let mut threads = threads.lock().unwrap();
-        for iter in 0..MAX_LOBBIES {
-            if threads[iter].is_none() {
-                {
-                    let (client_tx1, client_rx1) = mpsc::unbounded_channel();
-                    let (client_tx2, client_rx2) = mpsc::unbounded_channel();
-                    let (lobby_tx, lobby_rx) = mpsc::unbounded_channel();
-
-                    let mut lobby = lobby::Lobby::new();
-                    let thread = std::thread::spawn(move || {
-                        let rt = tokio::runtime::Builder::new_current_thread()
-                            .enable_all()
-                            .build()
-                            .unwrap();
-                        let _ = rt.block_on(async move { lobby.run(lobby_rx).await });
-                    });
-
-                    let _ = lobby_tx.send(S2L::NewClient(client_id, client_tx1, client_rx2));
-
-                    threads[iter] = Some(thread);
-                    match lobby_txs.lock() {
-                        Err(_) => {
-                            return Err(ServerErr::PoisonedMutex);
-                        }
-                        Ok(mut lobby_txs) => {
-                            lobby_txs[iter] = Some(lobby_tx);
-                            println!("Created a new lobby with space for the client");
-                            return Ok((client_id, client_tx2, client_rx1));
-                        }
-                    }
-                }
-            }
-        }
-        Err(ServerErr::ServerFull)
-    }
-
-    async fn wait_authentication(buf_reader: &mut BufReader<OwnedReadHalf>,) -> Result<String, ServerErr> {
-        let mut authentication = Err(ServerErr::AuthFailed);
+    
+    async fn wait_authentication(buf_reader: &mut BufReader<OwnedReadHalf>) -> Result<String, ServerErr> {
         tokio::select! {
+            biased;
             msg = stream::get_msg_from_client(buf_reader) => {
                 if let Ok(common::C2S::Login(name)) = msg {
-                    authentication = Ok(name);
+                    Ok(name)
+                } else {
+                    Err(ServerErr::AuthFailed)
                 }
             },
-            _ = tokio::time::sleep(tokio::time::Duration::from_millis(10000)) => {
+            _ = tokio::time::sleep(tokio::time::Duration::from_secs(10)) => {
+                Err(ServerErr::AuthFailed)
             }
-        };
-        authentication
+        }
     }
 }
