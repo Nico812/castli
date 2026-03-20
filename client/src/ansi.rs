7e1c9b8d5d4ee58ba0413c11728ce1a08de42d83
Nico812 <nicolo.hepp@protonmail.com>
2026-03-20 21:30:23 +0100

    weeee bellissimo

--- client/src/ansi.rs
+++ client/src/ansi.rs
@@ -5,43 +5,47 @@
 // === ANSI COLORS ===
 // Foreground colors
 pub const FG_BLACK: &str = "\x1b[30m";
-pub const FG_RED: &str = "\x1b[31m";
-pub const FG_GREEN: &str = "\x1b[32m";
-pub const FG_YELLOW: &str = "\x1b[33m";
+pub const FG_BLACK_BRIGHT: &str = "\x1b[90m";
 pub const FG_BLUE: &str = "\x1b[34m";
-pub const FG_MAGENTA: &str = "\x1b[35m";
+pub const FG_BLUE_BRIGHT: &str = "\x1b[94m";
+pub const FG_BROWN: &str = "\x1b[38;2;139;69;19m";
 pub const FG_CYAN: &str = "\x1b[36m";
+pub const FG_CYAN_BRIGHT: &str = "\x1b[96m";
+pub const FG_GREEN: &str = "\x1b[32m";
+pub const FG_GREEN_BRIGHT: &str = "\x1b[92m";
+pub const FG_GREEN_DARK: &str = "\x1b[38;2;0;100;0m";
+pub const FG_GREY: &str = "\x1b[38;2;128;128;128m";
+pub const FG_LIGHT_BROWN: &str = "\x1b[38;2;205;133;63m";
+pub const FG_MAGENTA: &str = "\x1b[35m";
+pub const FG_MAGENTA_BRIGHT: &str = "\x1b[95m";
+pub const FG_RED: &str = "\x1b[31m";
+pub const FG_RED_BRIGHT: &str = "\x1b[91m";
 pub const FG_WHITE: &str = "\x1b[37m";
+pub const FG_WHITE_BRIGHT: &str = "\x1b[97m";
+pub const FG_YELLOW: &str = "\x1b[33m";
+pub const FG_YELLOW_BRIGHT: &str = "\x1b[93m";
 
 // Background colors
 pub const BG_BLACK: &str = "\x1b[40m";
-pub const BG_RED: &str = "\x1b[41m";
-pub const BG_GREEN: &str = "\x1b[42m";
-pub const BG_YELLOW: &str = "\x1b[43m";
+pub const BG_BLACK_BRIGHT: &str = "\x1b[100m";
 pub const BG_BLUE: &str = "\x1b[44m";
-pub const BG_MAGENTA: &str = "\x1b[45m";
+pub const BG_BLUE_BRIGHT: &str = "\x1b[104m";
+pub const BG_BROWN: &str = "\x1b[48;2;139;69;19m";
 pub const BG_CYAN: &str = "\x1b[46m";
+pub const BG_CYAN_BRIGHT: &str = "\x1b[106m";
+pub const BG_GREEN: &str = "\x1b[42m";
+pub const BG_GREEN_BRIGHT: &str = "\x1b[102m";
+pub const BG_GREEN_DARK: &str = "\x1b[48;2;0;100;0m";
+pub const BG_GREY: &str = "\x1b[48;2;128;128;128m";
+pub const BG_LIGHT_BROWN: &str = "\x1b[48;2;205;133;63m";
+pub const BG_MAGENTA: &str = "\x1b[45m";
+pub const BG_MAGENTA_BRIGHT: &str = "\x1b[105m";
+pub const BG_RED: &str = "\x1b[41m";
+pub const BG_RED_BRIGHT: &str = "\x1b[101m";
 pub const BG_WHITE: &str = "\x1b[47m";
-
-// Bright foreground colors
-pub const FG_BRIGHT_BLACK: &str = "\x1b[90m";
-pub const FG_BRIGHT_RED: &str = "\x1b[91m";
-pub const FG_BRIGHT_GREEN: &str = "\x1b[92m";
-pub const FG_BRIGHT_YELLOW: &str = "\x1b[93m";
-pub const FG_BRIGHT_BLUE: &str = "\x1b[94m";
-pub const FG_BRIGHT_MAGENTA: &str = "\x1b[95m";
-pub const FG_BRIGHT_CYAN: &str = "\x1b[96m";
-pub const FG_BRIGHT_WHITE: &str = "\x1b[97m";
-
-// Bright background colors
-pub const BG_BRIGHT_BLACK: &str = "\x1b[100m";
-pub const BG_BRIGHT_RED: &str = "\x1b[101m";
-pub const BG_BRIGHT_GREEN: &str = "\x1b[102m";
-pub const BG_BRIGHT_YELLOW: &str = "\x1b[103m";
-pub const BG_BRIGHT_BLUE: &str = "\x1b[104m";
-pub const BG_BRIGHT_MAGENTA: &str = "\x1b[105m";
-pub const BG_BRIGHT_CYAN: &str = "\x1b[106m";
-pub const BG_BRIGHT_WHITE: &str = "\x1b[107m";
+pub const BG_WHITE_BRIGHT: &str = "\x1b[107m";
+pub const BG_YELLOW: &str = "\x1b[43m";
+pub const BG_YELLOW_BRIGHT: &str = "\x1b[103m";
 
 // === ANSI ELEMENTS AND CODES ===
 pub const RESET_COLOR: &str = "\x1b[0m";
--- client/src/assets.rs
+++ client/src/assets.rs
@@ -34,91 +34,95 @@ pub const CURSOR_DOWN: TermCell = TermCell::new('\u{21B3}', FG_WHITE, BG_BLACK);
 // pub const CURSOR_DOWN: TermCell = TermCell::new('\u{27B7}', FG_WHITE, BG_BLACK);
 
 pub const ERR_FG: &str = FG_MAGENTA;
-pub const ERR_BG: &str = BG_BRIGHT_MAGENTA;
-pub const ERR_EL: TermCell = TermCell::new('?', FG_MAGENTA, BG_BRIGHT_MAGENTA);
+pub const ERR_BG: &str = BG_MAGENTA_BRIGHT;
+pub const ERR_EL: TermCell = TermCell::new('?', FG_MAGENTA, ERR_BG);
 pub const BKG_FG: &str = FG_BLACK;
 pub const BKG_BG: &str = BG_BLACK;
 pub const BKG_EL: TermCell = TermCell::new('.', FG_RED, BG_BLACK);
 
 pub const GRASS_FG: &str = FG_GREEN;
 pub const GRASS_BG: &str = BG_GREEN;
-pub const GRASS_EL_1: TermCell = TermCell::new(' ', FG_BRIGHT_GREEN, GRASS_BG);
-pub const GRASS_EL_2: TermCell = TermCell::new('\"', FG_BRIGHT_GREEN, GRASS_BG);
+pub const GRASS_EL_1: TermCell = TermCell::new(' ', FG_GREEN_BRIGHT, GRASS_BG);
+pub const GRASS_EL_2: TermCell = TermCell::new('\"', FG_GREEN_BRIGHT, GRASS_BG);
 
 pub const WATER_FG: &str = FG_BLUE;
 pub const WATER_BG: &str = BG_BLUE;
-pub const WATER_EL_1: TermCell = TermCell::new(' ', FG_BRIGHT_BLUE, WATER_BG);
-pub const WATER_EL_2: TermCell = TermCell::new('~', FG_BRIGHT_BLUE, WATER_BG);
+pub const WATER_EL_1: TermCell = TermCell::new(' ', FG_BLUE_BRIGHT, WATER_BG);
+pub const WATER_EL_2: TermCell = TermCell::new('~', FG_BLUE_BRIGHT, WATER_BG);
 
-pub const WOODS_FG: &str = FG_RED;
-pub const WOODS_BG: &str = BG_RED;
-pub const WOODS_EL_1: TermCell = TermCell::new('#', FG_BRIGHT_RED, WOODS_BG);
-pub const WOODS_EL_2: TermCell = TermCell::new('#', FG_BRIGHT_YELLOW, WOODS_BG);
+pub const WOODS_FG: &str = FG_GREEN_DARK;
+pub const WOODS_BG: &str = BG_GREEN_DARK;
+pub const WOODS_EL_1: TermCell = TermCell::new(' ', FG_GREEN, WOODS_BG);
+pub const WOODS_EL_2: TermCell = TermCell::new('#', FG_WHITE, WOODS_BG);
 
 pub const MOUNTAIN_FG: &str = FG_GREEN;
 pub const MOUNTAIN_BG: &str = BG_GREEN;
-pub const MOUNTAIN_EL_1: TermCell = TermCell::new('#', FG_BRIGHT_RED, MOUNTAIN_BG);
-pub const MOUNTAIN_EL_2: TermCell = TermCell::new('#', FG_BRIGHT_YELLOW, MOUNTAIN_BG);
+pub const MOUNTAIN_EL_1: TermCell = TermCell::new(' ', FG_RED_BRIGHT, MOUNTAIN_BG);
+pub const MOUNTAIN_EL_2: TermCell = TermCell::new('#', FG_WHITE, MOUNTAIN_BG);
 
 pub const HIGH_MOUNTAIN_FG: &str = FG_GREEN;
 pub const HIGH_MOUNTAIN_BG: &str = BG_GREEN;
-pub const HIGH_MOUNTAIN_EL_1: TermCell = TermCell::new('#', FG_BRIGHT_RED, HIGH_MOUNTAIN_BG);
-pub const HIGH_MOUNTAIN_EL_2: TermCell = TermCell::new('#', FG_BRIGHT_YELLOW, HIGH_MOUNTAIN_BG);
+pub const HIGH_MOUNTAIN_EL_1: TermCell = TermCell::new('#', FG_RED_BRIGHT, HIGH_MOUNTAIN_BG);
+pub const HIGH_MOUNTAIN_EL_2: TermCell = TermCell::new('#', FG_YELLOW_BRIGHT, HIGH_MOUNTAIN_BG);
 
 pub const CASTLE_FG: &str = FG_WHITE;
 pub const CASTLE_BG: &str = BG_BLACK;
 
 pub const ERR_ART: &[&[TermCell]] = &[&[ERR_EL]];
 
-pub const CASTLE_ART: &[&[TermCell]] = &[
-    &[
-        TermCell::new('M', CASTLE_FG, CASTLE_BG),
-        TermCell::new('M', CASTLE_FG, CASTLE_BG),
-        TermCell::new('_', CASTLE_FG, CASTLE_BG),
-        TermCell::new('M', CASTLE_FG, CASTLE_BG),
-        TermCell::new('_', CASTLE_FG, CASTLE_BG),
-        TermCell::new('M', CASTLE_FG, CASTLE_BG),
-        TermCell::new('_', CASTLE_FG, CASTLE_BG),
-        TermCell::new('M', CASTLE_FG, CASTLE_BG),
-    ],
-    &[
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-    ],
-    &[
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new('_', CASTLE_FG, CASTLE_BG),
-        TermCell::new('_', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-    ],
-    &[
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-        TermCell::new(' ', CASTLE_FG, CASTLE_BG),
-        TermCell::new('|', CASTLE_FG, CASTLE_BG),
-    ],
-];
+// pub const CASTLE_ART: &[&[TermCell]] = &[
+//     &[
+//         TermCell::new('M', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('M', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('_', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('M', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('_', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('M', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('_', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('M', CASTLE_FG, CASTLE_BG),
+//     ],
+//     &[
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//     ],
+//     &[
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('_', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('_', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//     ],
+//     &[
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//         TermCell::new(' ', CASTLE_FG, CASTLE_BG),
+//         TermCell::new('|', CASTLE_FG, CASTLE_BG),
+//     ],
+// ];
+//
+// pub const CASTLE_ART_WORLD: &[&[TermCell]] = &[&[
+//     TermCell::new('C', FG_YELLOW, BG_BLACK),
+//     TermCell::new('C', FG_YELLOW, BG_BLACK),
+// ]];
 
-pub const CASTLE_ART_WORLD: &[&[TermCell]] = &[&[
-    TermCell::new('C', FG_YELLOW, BG_BLACK),
-    TermCell::new('C', FG_YELLOW, BG_BLACK),
-]];
+pub const CASTLE_ART: &[&[TermCell]] = &[&[TermCell::new('@', FG_WHITE_BRIGHT, BG_BLACK)]];
 
-pub const DEPLOYED_UNITS_ART: &[&[TermCell]] = &[&[TermCell::new('U', FG_RED, BG_BLACK)]];
+pub const CASTLE_ART_WORLD: &[&[TermCell]] = &[&[TermCell::new('@', FG_WHITE_BRIGHT, BG_BLACK)]];
+
+pub const DEPLOYED_UNITS_ART: &[&[TermCell]] = &[&[TermCell::new('u', FG_RED, BG_BLACK)]];
 
 pub const ERR_ART_SIZE: (usize, usize) = (ERR_ART.len(), ERR_ART[0].len());
 
